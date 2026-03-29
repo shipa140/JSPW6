c65c89e048e1510ede683fb56b1b672b8d150c05
shipa140 <89710591+shipa140@users.noreply.github.com>
2026-03-29 10:56:34 +0300

    fix tests and add patterns

--- src/pages/yourfeed.page.js
+++ src/pages/yourfeed.page.js
@@ -1,27 +1,42 @@
 export class YourFeedPage {
-
-    constructor (page)
-     {
+    constructor(page) {
         this.page = page;
-
         this.profileName = page.getByRole('navigation');
-        this.tags = page.getByRole('button', { name: 'реклама' });
-        this.feedtags = page.locator('li.nav-item button:has(i.ion-pound):has-text("реклама")');
-     }
+        this.feedtags = page.locator('.tag-list .tag-pill:first-child');
+        this.articles = page.locator('.article-preview');
+        this.articleTags = page.locator('.article-preview .tag-list .tag-pill');
+        this.emptystate = page.locator('.article-preview');
+    }
 
     async getProfileName() {
         return this.profileName;
     }
 
     async tagsfeed() {
-        await this.tags.click();
-
+        await this.feedtags.click();
     }
 
     async getFeedTags() {
         return this.feedtags;
     }
 
+    async getFirstTagText() {
+        await this.feedtags.waitFor({ state: 'visible' });
+        return await this.feedtags.innerText();
+    }
+
+    async clickFirstTag() {
+        await this.feedtags.waitFor({ state: 'visible' });
+        await this.feedtags.click();
+    }
+
+    async getArticleTagsText() {
+        await this.articles.first().waitFor({ state: 'visible' });
+        await this.page.waitForLoadState('networkidle');
+        return await this.articleTags.allInnerTexts();
+    }
+
+    emptyState() {
+        return this.emptystate;
+    }
 }
-
-
